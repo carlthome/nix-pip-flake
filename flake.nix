f5ed142e690466fcf1673e40b420ac86aaa03190
Carl Thomé <carlthome@gmail.com>
2022-12-14 20:22:36 +0100

    Add x86_64-linux

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A flake for using Python with pip inside a virtualenv";
   outputs = { self, nixpkgs }: {
-    packages.aarch64-darwin.default =
-      import ./default.nix { pkgs = nixpkgs.legacyPackages.aarch64-darwin; };
+    packages.aarch64-darwin.default = import ./default.nix { pkgs = nixpkgs.legacyPackages.aarch64-darwin; };
+    packages.x86_64-linux.default = import ./default.nix { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
   };
 }
